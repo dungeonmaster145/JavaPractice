ca6f187abcac11b26939e035bb554838ddb0987c
Mayank Joshi <mayank2420@gmail.com>
2021-07-21 20:37:53 +0530

    First Commit

---
+++ src/com/ArrayPrcatice/ThreeLargestElements.java
@@ -0,0 +1,30 @@
+package com.ArrayPrcatice;
+
+public class ThreeLargestElements {
+
+    public static void main (String[] args) {
+
+       int arr[] = {10, 4, 3, 50, 23, 90};
+
+       int first=Integer.MIN_VALUE;
+       int second=Integer.MIN_VALUE;
+       int third=Integer.MIN_VALUE;
+
+       for(int i=0;i<arr.length;i++){
+
+           if(arr[i]>first){
+               third=second;
+               second=first;
+               first=arr[i];
+           }
+           else if(arr[i]>second){
+               third=second;
+               second=arr[i];
+           }
+           else if(arr[i]>third){
+               third=arr[i];
+           }
+       }
+        System.out.println(first+" "+second+" "+third);
+    }
+}
---
+++ src/com/ArrayPrcatice/ZeroSumSubArray.java
@@ -0,0 +1,13 @@
+package com.ArrayPrcatice;
+
+import java.util.HashSet;
+
+public class ZeroSumSubArray {
+
+    public static void main (String[] args) {
+
+        int arr[]={4, 2, -3, 1, 6};
+        HashSet<Integer> hs=new HashSet<>();
+
+    }
+}
